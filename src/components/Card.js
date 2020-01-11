5edfaff08911708ae1babc8daeba7a1d3b934a1d
Tumo M <tumomasire@gmail.com>
2020-01-11 21:32:52 +0200

    Up and running!!! - Stlye Tweaks

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,8 @@ class Card extends React.Component {
     render(){
         const {name, email, id} = this.props.robots;
         return(
-            <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-                <img src={`https://robohash.org/${id}?200x200`} alt="A Robot"></img>
+            <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
+                <img src={`https://robohash.org/${id}?size=200x200`} alt="A Robot"></img>
                 <div>
                     <h2>{name}</h2>
                     <p>{email}</p>
