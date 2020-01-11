8e57c0cf220ba9d226f3088b7fc0485852ae7f84
Tumo M <tumomasire@gmail.com>
2020-01-11 20:30:36 +0200

    Updates

---
+++ precache-manifest.d136ad0b6e42cc25b4c128be89133ddc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e32dc3a54b19ccaadf4e91fdfea7324",
+    "url": "/RoboPals/index.html"
+  },
+  {
+    "revision": "9b475ea6bfdfe5acf169",
+    "url": "/RoboPals/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0a9a0b22db42ae1ff8c9",
+    "url": "/RoboPals/static/css/main.a508bdd1.chunk.css"
+  },
+  {
+    "revision": "9b475ea6bfdfe5acf169",
+    "url": "/RoboPals/static/js/2.484a5c16.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RoboPals/static/js/2.484a5c16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a9a0b22db42ae1ff8c9",
+    "url": "/RoboPals/static/js/main.4506cb1e.chunk.js"
+  },
+  {
+    "revision": "7674d98e81a830e82058",
+    "url": "/RoboPals/static/js/runtime-main.11a412a9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboPals/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robopals/precache-manifest.d6f303b77b331daadb362c785091caab.js"
+  "/RoboPals/precache-manifest.d136ad0b6e42cc25b4c128be89133ddc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robopals/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RoboPals/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
