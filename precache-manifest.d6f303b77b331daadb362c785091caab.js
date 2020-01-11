ad9af446d1919c070cc58556531ffea6dc066a60
Tumo M <tumomasire@gmail.com>
2020-01-11 20:27:05 +0200

    Updates

---
+++ precache-manifest.d6f303b77b331daadb362c785091caab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ac55e6bf0b86e0cdbec48fa70f509d",
+    "url": "/robopals/index.html"
+  },
+  {
+    "revision": "9b475ea6bfdfe5acf169",
+    "url": "/robopals/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8077095cc01139f3ed5b",
+    "url": "/robopals/static/css/main.4a62fce6.chunk.css"
+  },
+  {
+    "revision": "9b475ea6bfdfe5acf169",
+    "url": "/robopals/static/js/2.484a5c16.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robopals/static/js/2.484a5c16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8077095cc01139f3ed5b",
+    "url": "/robopals/static/js/main.5ab34256.chunk.js"
+  },
+  {
+    "revision": "558285a00000f9e1b47f",
+    "url": "/robopals/static/js/runtime-main.709fde9e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robopals/static/media/SEGA.12a3db8b.woff"
+  }
+]);
